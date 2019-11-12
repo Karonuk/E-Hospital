30099b1d3effa7fdee569b9e8afd85067d2ff35b
Nazaruk_a <Nazaruk_a@A12PC11>
2019-11-12 20:44:00 +0200

    feat(Client): added authentication functionality

--- E-Hospital/E-Hospital.Client/MainWindow.xaml.cs
+++ E-Hospital/E-Hospital.Client/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using System.Windows;
+using E_Hospital.Client.AuthService;
 
 namespace E_Hospital.Client
 {
@@ -8,9 +10,35 @@ namespace E_Hospital.Client
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            _authService = new AuthServiceClient();
+            
+        }
+
+        private readonly AuthServiceClient _authService;
+
+        private async void LogInButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(LoginTextBox.Text) || string.IsNullOrWhiteSpace(PasswordTextBox.Password))
+            {
+                await this.ShowMessageAsync("Warning", "Please fill login and password fields");
+                return;
+            }
+
+            var user = _authService.Login(LoginTextBox.Text,PasswordTextBox.Password);
+        }
+
+        private void RegisterAsDoctor_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegisterAsPatient_OnClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
