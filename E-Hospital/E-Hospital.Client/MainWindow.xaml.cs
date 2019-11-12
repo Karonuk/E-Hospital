72d57e500f906f44755670d8278b3df649f08f97
Nazaruk_a <Nazaruk_a@A12PC11>
2019-11-12 19:14:04 +0200

    feat(Client): created MainWindow layout

--- E-Hospital/E-Hospital.Client/MainWindow.xaml.cs
+++ E-Hospital/E-Hospital.Client/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace E_Hospital.Client
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
